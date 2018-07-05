04137ac83a617a0b7c447c99fdaebe80b4487adc
马成林 <machenglin@xiaomi.com>
2018-07-05 10:02:18 +0800

    fix

--- src/main/java/com/leetcode/array/ArrayLeetCode.java
+++ src/main/java/com/leetcode/array/ArrayLeetCode.java
@@ -10,7 +10,9 @@ import java.util.Arrays;
 public class ArrayLeetCode {
     public static void main(String[] args) {
         //rotate2(new int[]{-1},2);
-        System.out.println(singleNumber(new int[]{4,1,2,1,2}));
+        //System.out.println(singleNumber(new int[]{4,1,2,1,2}));
+        ArrayUtils.printArr(plusOne(new int[]{9}));
+
     }
     public  static  void rotate(int[] nums, int k) {
         for(int i = 1; i<=k;i++){
@@ -30,17 +32,44 @@ public class ArrayLeetCode {
     }
 
     public static int singleNumber(int[] nums) {
+        if (nums.length == 1) {
+            return nums[0];
+        }
         Arrays.sort(nums);
-        int result = nums[0];
-        for(int i =1;i<nums.length;i++){
-            if(nums[i] == result){
-                continue;
+        if(nums[nums.length-1]!= nums[nums.length-2]){
+            return nums[nums.length-1];
+        }
+
+        for(int i =1;i<nums.length-2;i++){
+            if(nums[i]!= nums[i-1] && nums[i]!=nums[i+1]){
+                return nums[i];
             }
-            result = nums[i];
-            if(i +1 < nums.length && result != nums[i+1] && ){
+
+        }
+        return 0;
+    }
+    //public int[] intersect(int[] nums1, int[] nums2) {
+    //
+    //}
+    public static int[] plusOne(int[] digits) {
+        if(digits[digits.length-1] != 9){
+            digits[digits.length-1] = digits[digits.length-1]+1;
+            return digits;
+        }
+        for(int i=digits.length-1;i>=0;i--){
+            if(digits[i] == 9){
+                digits[i] = 0;
+            } else {
+                digits[i]++;
+                return digits;
+            }
+            if(i == 0){
+                int[] result = new int[digits.length+1];
+                result[0]=1;
+                System.arraycopy(digits,0,result,1,digits.length);
                 return result;
             }
         }
-        return result;
+        return digits;
     }
 }
